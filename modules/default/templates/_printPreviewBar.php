19113d0661a96ab2997e3f26d3fad6f7899bf1e2
Tes Pheakdey <pheakdey.micronet@gmail.com>
2023-08-31 08:17:11 +0000

    update atom

--- modules/default/templates/_printPreviewBar.php
+++
@@ -1,6 +0,0 @@
-<?php if ('print' == $sf_request->getParameter('media')) { ?>
-  <div id="preview-message">
-    <?php echo __('Print preview'); ?>
-    <?php echo link_to(__('Close'), array_diff($sf_data->getRaw('sf_request')->getParameterHolder()->getAll(), ['media' => 'print'])); ?>
-  </div>
-<?php } ?>
